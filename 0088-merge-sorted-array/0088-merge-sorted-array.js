3e9c4b6b9ffcae464c1eced069e3acc99f541edb
LilyLin <linxl961203@gmail.com>
2025-12-10 19:40:07 +0800

    Time: 0 ms (100%), Space: 54.5 MB (70.28%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -5,7 +5,20 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function (nums1, m, nums2, n) {
-    nums1.splice(m,nums1.length-m,...nums2)
-    nums1.sort((a,b)=>a-b)
+var merge = function(nums1, m, nums2, n) {
+    let p1 = m - 1, p2 = n - 1;
+    let tail = m + n - 1;
+    var cur;
+    while (p1 >= 0 || p2 >= 0) {
+        if (p1 === -1) {
+            cur = nums2[p2--];
+        } else if (p2 === -1) {
+            cur = nums1[p1--];
+        } else if (nums1[p1] > nums2[p2]) {
+            cur = nums1[p1--];
+        } else {
+            cur = nums2[p2--];
+        }
+        nums1[tail--] = cur;
+    }
 };
